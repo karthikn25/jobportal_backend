5da9a8d814ad2c46086ef18d537ce475546847f3
karthikn25 <mkarthikeyan179@gmail.com>
2024-11-17 13:24:59 +0530

    first commit

--- Controller/auth.js
+++ Controller/auth.js
@@ -1,25 +1,59 @@
+// const jwt = require('jsonwebtoken');
+// const {User}=require('../Models/users.js');
+
+
+
+// const isAuth = async (req, res, next) => {
+//   let token;
+
+//   if (req.headers) {
+//     try {
+//       token = req.headers["x-auth-token"];
+//       const decode = jwt.verify(token, process.env.secretKey);
+//       req.user = await User.findById(decode.id).select("name email _id");
+//       next();
+//     } catch (error) {
+//       return res.status(400).json({ message: "Invalid Authorization" });
+//     }
+//     if (!token) {
+//       return res.staus(400).json({ message: "Access denied" });
+//     }
+//   }
+// };
+
+
+// module.exports={ isAuth };
 const jwt = require('jsonwebtoken');
-const {User}=require('../Models/users.js');
-
-
+const { User } = require('../Models/users.js');  // Importing User model
 
 const isAuth = async (req, res, next) => {
   let token;
 
-  if (req.headers) {
+  // Check if token is present in the request header
+  if (req.headers && req.headers["x-auth-token"]) {
     try {
       token = req.headers["x-auth-token"];
-      const decode = jwt.verify(token, process.env.secretKey);
-      req.user = await User.findById(decode.id).select("name email _id");
+
+      // Verify the token using the secret key
+      const decoded = jwt.verify(token, process.env.secretKey);
+
+      // Attach the user data to the request object
+      req.user = await User.findById(decoded.id).select("name email _id");
+
+      if (!req.user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Proceed to the next middleware/route handler
       next();
+
     } catch (error) {
-      return res.status(400).json({ message: "Invalid Authorization" });
+      console.log(error);
+      return res.status(400).json({ message: "Invalid Authorization" });  // Invalid token or error in decoding
     }
-    if (!token) {
-      return res.staus(400).json({ message: "Access denied" });
-    }
+  } else {
+    return res.status(400).json({ message: "Access denied. No token provided" });  // Token not provided
   }
 };
 
-
-module.exports={ isAuth };
+module.exports = { isAuth };
--- Routes/notes.js
+++ Routes/notes.js
@@ -1,28 +1,188 @@
-const express = require("express");
-const { Notes } = require("../Models/notes.js");
+// const express = require("express");
+// const { Notes } = require("../Models/notes.js");
+
+// const router = express.Router();
+
+// router.get("/all", async (req, res) => {
+//   try {
+//     const notes = await Notes.find().populate("user", "name email avatar");
+//     if (!notes) {
+//       return res.status(400).json({ message: "Notes not available" });
+//     }
+//     res.status(200).json({ message: "Successfully retrived Data", notes });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/getall/:id", async (req, res) => {
+//   try {
+//     const notes = await Notes.find({})
+//       .where({ user: { $ne: req.params.id } })
+//       .populate("user", "name email avatar");
+//     if (!notes) {
+//       return res.status(400).json({ message: "Couldn't fond any data" });
+//     }
+//     res.status(200).json({ message: "Successfully found your data", notes });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+
+
+// router.get("/getsingle/:id", async (req, res) => {
+//   try {
+//     const notes = await Notes.findOne({ _id: req.params.id }).populate(
+//       "user",
+//       "email name avatar"
+//     );
+//     if (!notes) {
+//       res.status(400).json({ message: "Error Occured in Data Found" });
+//     }
+//     res.status(200).json({ message: "Data found successfully", notes });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/user",async(req,res)=>{
+//   try {
+//     const notes = await Notes.find({user:req.user._id}).populate("user","name email avatar location position");
+//     if(!notes){
+//       res.status(400).json({message:"Data not available"})
+//     }
+//     res.status(200).json({message:"Data found Successfully",notes})
+    
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// })
+
+// router.get("/data", async (req, res) => {
+//   try {
+//     const notes = await Notes.find({ user: req.user._id }).populate(
+//       "user",
+//       "name email avatar"
+//     );
+//     if (!notes) {
+//       return res.status(400).json({ message: "Couldn't fond any data" });
+//     }
+//     res
+//       .status(200)
+//       .json({ message: "Successfully found your data", length: notes.length,notes });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// router.get("/search/:keyword",async(req,res)=>{
+//   try {
+//     const {keyword} = req.params;   
+//     const notes = await Notes.find({
+//       $or:[
+//         {companyName:{$regex:keyword, $options:"i"}},
+//         {position:{$regex:keyword, $options:"i"}},
+//         {location:{$regex:keyword, $options:"i"}}
+//       ]
+//     }).populate("user","name email avatar")
+//     if(!notes){
+//       res.status(400).json({message:"Data not Found"})
+//     }
+//     res.status(200).json({message:"Data Found Successfully",length:notes.length,notes})
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// })
+
+// router.post("/create", async (req, res) => {
+//   const postedDate = new Date().toJSON().slice(0, 10);
+
+//   try {
+    
+//     const notes = new Notes({
+//       ...req.body,
+//       date: postedDate,
+//       user: req.user
+      
+//     });
+//     await notes.save();
+//     notes.populate("user","name email avatar")
+//     res.status(201).json({message:"Data upload successfully",notes})
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
+
+// router.put("/edit/:id", async (req, res) => {
+//   try {
+//     const updatedNotes = await Notes.findOneAndUpdate(
+//       { _id: req.params.id },
+//       { $set: req.body },
+//       { new: true }
+//     );
+//     if (!updatedNotes) {
+//       return res.status(400).json({ message: "Error occured in Updation" });
+//     }
+//     res
+//       .status(200)
+//       .json({ message: "Successfully Updated", data: updatedNotes });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// router.delete("/remove/:id", async (req, res) => {
+//   try {
+//     const removeNotes = await Notes.findByIdAndDelete({ _id: req.params.id });
+//     if (!removeNotes) {
+//       return res
+//         .status(400)
+//         .json({ message: "Error occured in data Deletion" });
+//     }
+//     res.status(200).json({ message: "Data Remove Successfully" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// const notesRouter = router;
+
+// module.exports = { notesRouter };
+const express = require('express');
+const { Notes } = require('../Models/notes.js');
+const { isAuth } = require('../Controller/auth');  // Importing the auth middleware
 
 const router = express.Router();
 
+// Get all notes (no authentication required)
 router.get("/all", async (req, res) => {
   try {
     const notes = await Notes.find().populate("user", "name email avatar");
     if (!notes) {
       return res.status(400).json({ message: "Notes not available" });
     }
-    res.status(200).json({ message: "Successfully retrived Data", notes });
+    res.status(200).json({ message: "Successfully retrieved Data", notes });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-router.get("/getall/:id", async (req, res) => {
+// Get notes excluding the current user's notes (authenticated route)
+router.get("/getall/:id", isAuth, async (req, res) => {
   try {
-    const notes = await Notes.find({})
-      .where({ user: { $ne: req.params.id } })
-      .populate("user", "name email avatar");
+    const notes = await Notes.find({}).where({ user: { $ne: req.params.id } }).populate("user", "name email avatar");
     if (!notes) {
-      return res.status(400).json({ message: "Couldn't fond any data" });
+      return res.status(400).json({ message: "Couldn't find any data" });
     }
     res.status(200).json({ message: "Successfully found your data", notes });
   } catch (error) {
@@ -31,14 +191,10 @@ router.get("/getall/:id", async (req, res) => {
   }
 });
 
-
-
+// Get single note by ID
 router.get("/getsingle/:id", async (req, res) => {
   try {
-    const notes = await Notes.findOne({ _id: req.params.id }).populate(
-      "user",
-      "email name avatar"
-    );
+    const notes = await Notes.findOne({ _id: req.params.id }).populate("user", "email name avatar");
     if (!notes) {
       res.status(400).json({ message: "Error Occured in Data Found" });
     }
@@ -49,78 +205,29 @@ router.get("/getsingle/:id", async (req, res) => {
   }
 });
 
-router.get("/user",async(req,res)=>{
-  try {
-    const notes = await Notes.find({user:req.user._id}).populate("user","name email avatar location position");
-    if(!notes){
-      res.status(400).json({message:"Data not available"})
-    }
-    res.status(200).json({message:"Data found Successfully",notes})
-    
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-})
-
-router.get("/data", async (req, res) => {
-  try {
-    const notes = await Notes.find({ user: req.user._id }).populate(
-      "user",
-      "name email avatar"
-    );
-    if (!notes) {
-      return res.status(400).json({ message: "Couldn't fond any data" });
-    }
-    res
-      .status(200)
-      .json({ message: "Successfully found your data", length: notes.length,notes });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-router.get("/search/:keyword",async(req,res)=>{
-  try {
-    const {keyword} = req.params;   
-    const notes = await Notes.find({
-      $or:[
-        {companyName:{$regex:keyword, $options:"i"}},
-        {position:{$regex:keyword, $options:"i"}},
-        {location:{$regex:keyword, $options:"i"}}
-      ]
-    }).populate("user","name email avatar")
-    if(!notes){
-      res.status(400).json({message:"Data not Found"})
-    }
-    res.status(200).json({message:"Data Found Successfully",length:notes.length,notes})
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-})
-
-router.post("/create", async (req, res) => {
+// Create a new note (requires authentication)
+router.post("/create", isAuth, async (req, res) => {
   const postedDate = new Date().toJSON().slice(0, 10);
 
   try {
-    
-    const notes = new Notes({
+    const newNote = new Notes({
       ...req.body,
       date: postedDate,
-      user: req.user
-      
+      user: req.user._id  // Attach the authenticated user's ID to the note
     });
-    await notes.save();
-    notes.populate("user","name email avatar")
-    res.status(201).json({message:"Data upload successfully",notes})
+
+    await newNote.save();
+    await newNote.populate('user', 'name email _id');  // Populate user info (optional)
+
+    res.status(201).json({ message: 'Note created successfully', newNote });
   } catch (error) {
-    res.status(400).send(error);
+    console.error(error);
+    res.status(400).json({ message: 'Error creating note', error });
   }
 });
 
-router.put("/edit/:id", async (req, res) => {
+// Update note by ID (requires authentication)
+router.put("/edit/:id", isAuth, async (req, res) => {
   try {
     const updatedNotes = await Notes.findOneAndUpdate(
       { _id: req.params.id },
@@ -128,24 +235,21 @@ router.put("/edit/:id", async (req, res) => {
       { new: true }
     );
     if (!updatedNotes) {
-      return res.status(400).json({ message: "Error occured in Updation" });
+      return res.status(400).json({ message: "Error occurred in Updation" });
     }
-    res
-      .status(200)
-      .json({ message: "Successfully Updated", data: updatedNotes });
+    res.status(200).json({ message: "Successfully Updated", data: updatedNotes });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-router.delete("/remove/:id", async (req, res) => {
+// Delete note by ID (requires authentication)
+router.delete("/remove/:id", isAuth, async (req, res) => {
   try {
     const removeNotes = await Notes.findByIdAndDelete({ _id: req.params.id });
     if (!removeNotes) {
-      return res
-        .status(400)
-        .json({ message: "Error occured in data Deletion" });
+      return res.status(400).json({ message: "Error occurred in data Deletion" });
     }
     res.status(200).json({ message: "Data Remove Successfully" });
   } catch (error) {
@@ -154,6 +258,4 @@ router.delete("/remove/:id", async (req, res) => {
   }
 });
 
-const notesRouter = router;
-
-module.exports = { notesRouter };
+module.exports = { notesRouter: router };
